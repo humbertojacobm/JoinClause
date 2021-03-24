e0515925223f3dbe714c0ed33449b66fa6a04c2d
humberto.mori <humberto.mori@tektonlabs.com>
2021-03-23 23:25:53 -0500

    uisn groupinnerjoin

--- JoinClause/Program.cs
+++ JoinClause/Program.cs
@@ -1,12 +1,124 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Linq;
 namespace JoinClause
 {
     class Program
     {
+        class Product
+        {
+            public string Name { get; set; }
+            public int CategoryID { get; set; }
+        }
+
+        class Category
+        {
+            public string Name { get; set; }
+            public int ID { get; set; }
+        }
+
+        // Specify the first data source.
+        List<Category> categories = new List<Category>()
+        {
+            new Category {Name="Beverages", ID=001},
+            new Category {Name="Condiments", ID=002},
+            new Category {Name="Vegetables", ID=003},
+            new Category {Name="Grains", ID=004},
+            new Category {Name="Fruit", ID=005}
+        };
+
+        // Specify the second data source.
+        List<Product> products = new List<Product>()
+        {
+            new Product {Name="Cola",  CategoryID=001},
+            new Product {Name="Tea",  CategoryID=001},
+            new Product {Name="Mustard", CategoryID=002},
+            new Product {Name="Pickles", CategoryID=002},
+            new Product {Name="Carrots", CategoryID=003},
+            new Product {Name="Bok Choy", CategoryID=003},
+            new Product {Name="Peaches", CategoryID=005},
+            new Product {Name="Melons", CategoryID=005},
+            };
         static void Main(string[] args)
         {
+            Program app = new Program();
+
+            app.InnerJoin();
+            app.GroupJoin();
+            app.GroupInnerJoin();
+
             Console.WriteLine("Hello World!");
+        }
+
+        void InnerJoin()
+        {
+            var innerJoinQuery = 
+                from category in categories
+                join prod in products on category.ID equals prod.CategoryID
+                select new { Category = category.ID, Product = prod.Name};
+
+            Console.WriteLine("InnerJoin:");
+
+            foreach(var item in innerJoinQuery)
+            {
+                Console.WriteLine("{0, -10}{1}",item.Product, item.Category);
+            }
+
+            Console.WriteLine("InnerJoin: {0} items in 1 group.", innerJoinQuery.Count());
+            Console.WriteLine(System.Environment.NewLine);
+
+        }
+
+        void GroupJoin(){
+            var groupJoinQuery = 
+                from category in categories
+                join prod in products on category.ID equals prod.CategoryID into prodGroup
+                select prodGroup;
+            
+            int totalItems = 0;
+
+            Console.WriteLine("Simple GroupJoin:");
+
+            foreach (var prodGrouping in groupJoinQuery)
+            {
+                Console.WriteLine("Group:");
+                foreach (var item in prodGrouping)
+                {
+                    totalItems++;
+                    Console.WriteLine("     {0,-10}{1}", item.Name, item.CategoryID);
+                }
+                Console.WriteLine("Unshaped GroupJoin: {0} items in {1} unnamed groups", totalItems,groupJoinQuery.Count());
+                Console.WriteLine(System.Environment.NewLine);
+            }
+        }
+        void GroupInnerJoin()
+        {
+            var groupJoinQuery2 =
+                from category in categories
+                orderby category.ID
+                join prod in products on category.ID equals prod.CategoryID into prodGroup
+                select new 
+                {
+                    Category = category.Name,
+                    Products = from prod2 in prodGroup
+                                orderby prod2.Name
+                                select prod2
+                };
+                //Console.WriteLine("GroupInnerJoin:);
+            int totalItems = 0;
+
+            Console.WriteLine("GroupInnerJoin:");
+            foreach (var productGroup in groupJoinQuery2)
+            {
+                Console.WriteLine(productGroup.Category);
+                foreach (var prodItem in productGroup.Products)
+                {
+                    totalItems++;
+                    Console.WriteLine("  {0,-10} {1}", prodItem.Name, prodItem.CategoryID);
+                }
+            }
+            Console.WriteLine("GroupInnerJoin: {0} items in {1} named groups", totalItems, groupJoinQuery2.Count());
+            Console.WriteLine(System.Environment.NewLine);
         }
     }
 }
